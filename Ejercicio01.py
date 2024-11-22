b37f8416a96e5f5b7176997cf1572075773d60e0
jlopezlash <jlopezlash1@educacion.navarra.es>
2024-11-22 08:49:19 +0100

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,5 @@
+#Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'},
+#pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
+Dinero = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+divisa = input('Dime tu divisa ')
+print(Dinero.get(divisa,'No Disponible '))
